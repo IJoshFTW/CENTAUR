28c2ff0595f161a1a4bc9b1d93074ab2e818f2f8
Joshua Slik <joshua.b.slik@gmail.com>
2014-11-20 12:22:10 +0100

    Cleanup and basic layout of XML File and Tag
    * Cleaned up temporary test file 'parsePlayerFiles' and removed the call from App to this class
    
    Conflicts:
    	src/main/java/nl/joshuaslik/UFMReckoning/App.java
    	src/main/java/nl/joshuaslik/UFMReckoning/XMLToolkit/XMLFile.java
    	src/main/java/nl/joshuaslik/UFMReckoning/XMLToolkit/XMLTag.java

---
+++ src/main/java/nl/joshuaslik/UFMReckoning/App.java
@@ -0,0 +1,12 @@
+package nl.joshuaslik.UFMReckoning;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		System.out.println("One day, this will be a cool Footbal Manager!");
+		
+	}
+	
+}
---
+++ src/main/java/nl/joshuaslik/UFMReckoning/XMLToolkit/XMLFile.java
@@ -0,0 +1,19 @@
+package nl.joshuaslik.UFMReckoning.XMLToolkit;
+
+/**
+ *
+ * @author <a href="http://www.joshuaslik.nl/" target="_blank">Joshua Slik</a>
+ *
+ */
+public class XMLFile {
+	
+	XMLTag mastertag = new XMLTag();
+	
+	/**
+	 * Constructor
+	 */
+	public XMLFile() {
+		
+	}
+	
+}
